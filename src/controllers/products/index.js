a769a1fc6daa5cfa308d3f8fa6f703fd601ffdf6
VisualStyle <andrus1997@hotmail.com>
2023-09-27 15:51:07 -0400

    crud productos con schema

---
+++ src/controllers/products/index.js
@@ -0,0 +1,55 @@
+const Product = require("../../schemas/Product");
+
+const getProducts = async (req, res) => {
+    const products = await Product.find()
+    res.json(products)
+};
+const createProduct = async (req, res) => {
+  const {
+    brand,
+    model,
+    price,
+    image,
+    category,
+    type,
+    stock,
+  } = req.body;
+
+  const newProduct = new Product({
+    brand,
+    model,
+    price,
+    image,
+    category,
+    type,
+    stock,
+  });
+
+  const savedProduct = await newProduct.save();
+  res.json(savedProduct);
+};
+const getProduct = async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if(!product) return res.status(404).json({message: 'Product not found'})
+    res.json(product)
+};
+const deleteProduct = async (req, res) => {
+    const product = await Product.findByIdAndDelete(req.params.id)
+    if(!product) return res.status(404).json({message: 'Product not found'})
+    res.status(204).json({message: 'Nothing to show'})
+};
+const updateProduct = async (req, res) => {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+    if(!product) return res.status(404).json({message: 'Product not found'})
+    res.json(product)
+};
+
+module.exports = {
+    getProduct,
+    getProducts,
+    createProduct,
+    deleteProduct,
+    updateProduct
+}
--- src/schemas/Product.js
+++ src/schemas/Product.js
@@ -1,26 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  brand: String,
-  model: String,
-  size: Number,
-  color: String,
-  gender: {
-    type: String,
-    enum: ['male', 'female']
+  brand: { type: String },
+  model: { type: String },
+  price: { type: Number },
+  image: {
+    type: [
+      {
+        id: { type: String },
+        src: { type: String },
+        typeImage: { type: String },
+        size: { type: Number },
+        color: { type: String },
+      },
+    ],
   },
-  image: [String],
-  category: [String],
-  type: [String],
+  category: { type: String },
+  type: { type: String },
   stock: {
-    size: String,
-    gender: String,
-    color: String,
-    price: Number,
-    amount: Number
-  }
+    type: [
+      {
+        size: Number,
+        gender: {
+          type: String,
+          enum: ["male", "female"],
+        },
+        color: [String],
+        quantity: Number,
+      },
+    ],
+  },
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
