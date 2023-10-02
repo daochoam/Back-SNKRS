40a9dd1d42832e431c4e78c93885ba9b703be45e
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2023-10-02 11:58:06 -0500

    Add Price and image en Schema Trolley

--- src/schemas/Trolley.js
+++ src/schemas/Trolley.js
@@ -17,8 +17,20 @@ const trolleySchema = new Schema({
                 },
                 color: { type: String },
                 size : { type: Number },
+                price: { type: Number },
                 quantity : { type: Number },
                 gener    : { type: String, enum: ["female", "male", "kid"] },
+                image   : {
+                    type: [
+                      {
+                        id  :  { type: String },
+                        src :  { type: String },
+                        size:  { type: Number },
+                        // color: { type: String },
+                        typeImage: { type: String },
+                      },
+                    ],
+                },
             }],
             required : true
         },
