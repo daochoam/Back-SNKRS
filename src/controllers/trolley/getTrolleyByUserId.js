2b1328a77ef3b1674d2b52605acc7976e2231502
Ana María Orozco <103206919+AnaO-97@users.noreply.github.com>
2023-10-03 18:27:17 -0500

    Add validateAuthUserSession in trolleyRoutes (#29)
    Co-authored-by: ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>

--- src/controllers/trolley/getTrolleyByUserId.js
+++ src/controllers/trolley/getTrolleyByUserId.js
@@ -2,9 +2,8 @@ const { Trolley } = require("../../schemas/index");
 
 const getTrolleyByUserId = async (req, res) => {
     try {
-
-        // const { User_id } = req.locals;
-        const User_id = "6517088344d46facf8d90480";
+        const { User_id } = req.locals;
+        // const User_id = "6517088344d46facf8d90480";
 
         const [ trolley ] = await Trolley.find({ User_id });
 
