88635983c7d90a9d007a3332c48f91309e83f882
Miguel <miguelfalconserquen@gmail.com>
2023-10-04 01:15:32 -0500

    un montón de cosas que hicieron Ana y Karol

--- src/controllers/trolley/updatePickByPickId.js
+++ src/controllers/trolley/updatePickByPickId.js
@@ -1,36 +1,36 @@
 const { Trolley } = require("../../schemas/index");
 
-const updatePickByPickId = async ( req, res ) => {
+const updatePickByPickId = async (req, res) => {
     try {
         const { User_id } = req.locals;
         // const User_id = "6517088344d46facf8d90480";
-        
+
         const { idPick } = req.params;
         const attributes = req.body;
-    
-        let   pickFound = {};
-        const [ trolleyBefore ] = await Trolley.find({"User_id": User_id})    
 
-        trolleyBefore.pickedProducts.forEach( pick => {
-            if (pick._id == idPick){               
+        let pickFound = {};
+        const [trolleyBefore] = await Trolley.find({ "User_id": User_id })
+
+        trolleyBefore.pickedProducts.forEach(pick => {
+            if (pick._id == idPick) {
                 const update = Object.entries(attributes);
-                update.forEach( att => pick[att[0]] = att[1] )
+                update.forEach(att => pick[att[0]] = att[1])
                 pickFound = pick;
-            }            
+            }
         });
 
-        if (pickFound){
+        if (pickFound) {
             const trolleyAfter = await trolleyBefore.save();
-    
+
             if (trolleyAfter)
                 res.status(200).json(pickFound);
             else
                 res.status(400).json({ "erro": "The pick was not updated correctly" });
         }
-        else{
+        else {
             res.status(400).json({ "error": "The pick was not found, please check the -->idPick<--" });
         }
-        
+
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
--- src/controllers/trolley/updateQuantityByPickId.js
+++ src/controllers/trolley/updateQuantityByPickId.js
@@ -1,30 +1,30 @@
 const { Trolley } = require('../../schemas/index');
 
-const updateQuantityByPickId = async ( req, res ) => {
+const updateQuantityByPickId = async (req, res) => {
     try {
         const { User_id } = req.locals;
         // const User_id = "6517088344d46facf8d90480";
 
         const { operation, idPick } = req.params;
 
-        if( operation === "plus" || operation === "less"){
-            let pickFound =  {};
-            
-            let [ trolleyBefore ] = await Trolley.find({"User_id" : User_id});
-            
-            trolleyBefore.pickedProducts.forEach(( pick ) => {
-                if(pick._id == idPick){
+        if (operation === "plus" || operation === "less") {
+            let pickFound = {};
+
+            let [trolleyBefore] = await Trolley.find({ "User_id": User_id });
+
+            trolleyBefore.pickedProducts.forEach((pick) => {
+                if (pick._id == idPick) {
                     pick.quantity = operation === "plus"
-                    ? pick.quantity + 1
-                    : pick.quantity - 1
-                
+                        ? pick.quantity + 1
+                        : pick.quantity - 1
+
                     pickFound = pick;
                 }
             })
 
-            if (pickFound){
+            if (pickFound) {
                 const trolleyAfter = await trolleyBefore.save();
-        
+
                 if (trolleyAfter)
                     res.status(200).json(trolleyAfter.pickedProducts);
                 else
@@ -32,13 +32,13 @@ const updateQuantityByPickId = async ( req, res ) => {
             }
             else
                 res.status(400).json({ "erro": "The pick was not found" });
-        }                    
+        }
         else
-            res.status(400).json({ "error": `The operation -->${operation}<-- is not valid` });        
-        
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+            res.status(400).json({ "error": The operation -- > ${ operation } < --is not valid });
+
+} catch (error) {
+    res.status(400).json({ error: error.message });
+}
 };
 
 module.exports = updateQuantityByPickId;
