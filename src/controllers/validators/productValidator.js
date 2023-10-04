0281b8cbcea6fecfe51e7000ae09e241b41eaad9
VisualStyle <andrus1997@hotmail.com>
2023-10-04 15:51:22 -0400

    validaciones terminadas

--- src/controllers/validators/productValidator.js
+++ src/controllers/validators/productValidator.js
@@ -1,49 +1,113 @@
-const { body } = require('express-validator');
+const { body } = require("express-validator");
 
 exports.createProductValidationRules = [
-  body('brand')
+  body("brand")
     .trim()
     .isLength({ min: 1 })
-    .withMessage('Brand is required')
+    .withMessage("Brand is required")
     .isLength({ max: 50 })
-    .withMessage('Brand must not exceed 50 characters')
+    .withMessage("Brand must not exceed 50 characters")
     .matches(/^[a-zA-Z0-9\s]+$/)
-    .withMessage('Brand can only contain letters, numbers, and spaces'),
-  body('model')
+    .withMessage("Brand can only contain letters, numbers, and spaces"),
+  body("model")
     .trim()
     .isLength({ min: 1 })
-    .withMessage('Model is required')
+    .withMessage("Model is required")
     .isLength({ max: 50 })
-    .withMessage('Model must not exceed 50 characters')
+    .withMessage("Model must not exceed 50 characters")
     .matches(/^[a-zA-Z0-9\s]+$/)
-    .withMessage('Model can only contain letters, numbers, and spaces'),
-  body('price')
+    .withMessage("Model can only contain letters, numbers, and spaces"),
+  body("price")
     .isNumeric()
-    .withMessage('Price must be a valid number')
+    .withMessage("Price must be a valid number")
     .isFloat({ min: 0 })
-    .withMessage('Price must be a positive value or zero'),
+    .withMessage("Price must be a positive value or zero"),
+  body("image")
+    .isArray()
+    .withMessage("Image must be an array")
+    .notEmpty()
+    .withMessage("Image cannot be empty")
+    .isArray({ min: 1 })
+    .withMessage("At least one image must be provided")
+    .custom((images) => {
+      return images.every(
+        (image) =>
+          image.hasOwnProperty("id") &&
+          image.hasOwnProperty("src") &&
+          image.hasOwnProperty("typeImage") &&
+          image.hasOwnProperty("size") &&
+          image.hasOwnProperty("color")
+      );
+    }),
+  body("stock")
+    .isArray()
+    .withMessage("Stock must be an array")
+    .notEmpty()
+    .withMessage("Stock cannot be empty")
+    .isArray({ min: 1 })
+    .withMessage("At least one stock item must be provided")
+    .custom((stock) => {
+      return stock.every(
+        (item) =>
+          item.hasOwnProperty("size") &&
+          item.hasOwnProperty("gender") &&
+          item.hasOwnProperty("color") &&
+          item.hasOwnProperty("quantity")
+      );
+    }),
 ];
 
 exports.updateProductValidationRules = [
-  body('brand')
+  body("brand")
     .trim()
     .isLength({ min: 1 })
-    .withMessage('Brand is required')
+    .withMessage("Brand is required")
     .isLength({ max: 50 })
-    .withMessage('Brand must not exceed 50 characters')
+    .withMessage("Brand must not exceed 50 characters")
     .matches(/^[a-zA-Z0-9\s]+$/)
-    .withMessage('Brand can only contain letters, numbers, and spaces'),
-  body('model')
+    .withMessage("Brand can only contain letters, numbers, and spaces"),
+  body("model")
     .trim()
     .isLength({ min: 1 })
-    .withMessage('Model is required')
+    .withMessage("Model is required")
     .isLength({ max: 50 })
-    .withMessage('Model must not exceed 50 characters')
+    .withMessage("Model must not exceed 50 characters")
     .matches(/^[a-zA-Z0-9\s]+$/)
-    .withMessage('Model can only contain letters, numbers, and spaces'),
-  body('price')
+    .withMessage("Model can only contain letters, numbers, and spaces"),
+  body("price")
     .isNumeric()
-    .withMessage('Price must be a valid number')
+    .withMessage("Price must be a valid number")
     .isFloat({ min: 0 })
-    .withMessage('Price must be a positive value or zero'),
+    .withMessage("Price must be a positive value or zero"),
+  body("image")
+    .optional()
+    .isArray()
+    .withMessage("Image must be an array")
+    .isArray({ min: 1 })
+    .withMessage("At least one image must be provided")
+    .custom((images) => {
+      return images.every(
+        (image) =>
+          image.hasOwnProperty("id") &&
+          image.hasOwnProperty("src") &&
+          image.hasOwnProperty("typeImage") &&
+          image.hasOwnProperty("size") &&
+          image.hasOwnProperty("color")
+      );
+    }),
+  body("stock")
+    .optional()
+    .isArray()
+    .withMessage("Stock must be an array")
+    .isArray({ min: 1 })
+    .withMessage("At least one stock item must be provided")
+    .custom((stock) => {
+      return stock.every(
+        (item) =>
+          item.hasOwnProperty("size") &&
+          item.hasOwnProperty("gender") &&
+          item.hasOwnProperty("color") &&
+          item.hasOwnProperty("quantity")
+      );
+    }),
 ];
