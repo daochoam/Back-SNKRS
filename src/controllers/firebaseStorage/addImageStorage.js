b62eea5d3856600edc5d7352314ec768b82dbe37
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2023-10-13 12:12:20 -0500

    Fix and Add AddImageStorage to createProduct Route

--- src/controllers/firebaseStorage/addImageStorage.js
+++ src/controllers/firebaseStorage/addImageStorage.js
@@ -1,29 +1,48 @@
-const app    = require("../../config/firebaseConfig");
 const { v4 } = require("uuid");
+const { initializeApp } = require("firebase/app");
 const { ref, getStorage, uploadBytes, getDownloadURL } = require("firebase/storage");
 
-const addImageStorage = async ( file, refFile = "images" ) => {
-    try {
-        console.log(file);
+// const { firebase } = require("../../config/firebaseConfig");
+// const { applicationDefault } = require('firebase-admin/app');
+
+const addImageStorage = async ( imagesFiles, colorImage, refFileStorage = "images" ) => {
+    try {                    
+        const firebaseConfig = {
+            apiKey: "AIzaSyBqdSo4w-A-p3icO9Ru4ZRBO4eEY68FCsM",
+            authDomain: "snkrs-4a559.firebaseapp.com",
+            projectId: "snkrs-4a559",
+            storageBucket: "snkrs-4a559.appspot.com",
+            messagingSenderId: "341444316031",
+            appId: "1:341444316031:web:3e6b273044504ca58aa84a"
+        };
+          
+        const app = initializeApp(firebaseConfig);
+        const storage    = getStorage(app);        
+
+        let image = {};
+        let imagesResponse = [];
+
+        for (const file of imagesFiles) {
+            const idImage    = v4();
+            const metadata   = { contentType: file.mimetype };
+            const imageToUint8 = new Uint8Array(file.buffer);
+
+            // const storageRef = ref(storage, `${refFileStorage}/${idImage}`);
+            const storageRef = ref(storage, idImage);
+    
+            const fileStorageFirebase = await (uploadBytes( storageRef, imageToUint8, metadata ));
+            const urlFirebase         = await (getDownloadURL(storageRef));
+
+            image = {
+                id   : idImage,
+                src  : urlFirebase,
+                color: colorImage,
+            };
+
+            imagesResponse.push(image);
+        }            
         
-        return file;
-        // const storage = getStorage(app);
-        // const idImage = v4();
-
-        // const storageRef = ref(storage, `${refFile}/${idImage}`);
-        
-        // const fileStorageFirebase = await uploadBytes( storageRef, file );
-        // const urlFirebase         = getDownloadURL(storageRef);
-
-        // const image = {
-        //     id   : idImage,
-        //     src  : urlFirebase,
-        //     size : fileStorageFirebase.size,
-        //     typeImage : fileStorageFirebase.type,
-        // }
-
-        // console.log("addImageStorage-22", image);
-        // return image;
+        return imagesResponse;
 
     } catch (error) {
         throw new Error(error);
