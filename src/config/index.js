96de04f2b2303da217404755f859ec2bf5912fa9
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2023-10-14 15:44:40 -0500

    Add firebase storage key protection

--- src/config/index.js
+++ src/config/index.js
@@ -3,11 +3,13 @@ const config = require('./config')
 const firebase = require('./firebaseConfig')
 const mailTransport = require('./mailTransportConfig')
 const swaggerSpec = require('./swaggerSpecConfig')
+const app = require('./firebaseStorageConfig');
 
 module.exports = {
     db,
     config,
     firebase,
     mailTransport,
-    swaggerSpec
+    swaggerSpec,
+    app
 }
