d395c7e749802ece181407dac2c93a0a96ed8006
daochoam <daochoam@unal.edu.co>
2023-10-19 13:29:11 -0500

    Upload Confirmation Image ANA!!!!!

--- src/controllers/products/updateImageProduct.js
+++ src/controllers/products/updateImageProduct.js
@@ -5,15 +5,16 @@ const updateImageProduct = async (req, res) => {
   try {
     const imagesFiles = req.files;
     const { imagesColors } = req.body;
-    
-    if(imagesFiles.length>0){
-      const imagesShoes   = await addImageStorage(imagesFiles, imagesColors, "createProduct");      
+
+    if (imagesFiles.length > 0) {
+      const imagesShoes = await addImageStorage(imagesFiles, imagesColors, "createProduct");
       const productBefore = await Product.findById(req.params.id);
       productBefore.image = imagesShoes;
 
-      const productAfter = await productBefore.save();    
-    } 
-    else{
+      const productAfter = await productBefore.save();
+      if (imagesShoes && productAfter) res.status(200).json(true)
+    }
+    else {
       res.status(400).json({ "error": "The image file was not received" });
     }
 
