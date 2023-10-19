cd2a2b9d1d98148f70dc2d89d085a1ebe5cc32f9
Miguel <miguelfalconserquen@gmail.com>
2023-10-18 22:55:37 -0500

    filtros

--- src/controllers/admin/getAllUser.js
+++ src/controllers/admin/getAllUser.js
@@ -2,7 +2,15 @@ const { User } = require("../../schemas/index");
 
 const getAllUser = async (req, res) => {
     try {
-        const users = await User.find();
+        const { firstName, email, country, status } = req.query;
+        const query = {};
+        const sort = {};
+        if (country) query["address.0.country"] = new RegExp(country, 'i');
+        if (status) query.status = new RegExp(status, 'i');
+        if (firstName) sort.firstName = firstName;
+        if (email) sort.email = email;
+
+        const users = await User.find(query).sort(sort);
         res.status(200).json(users);
     } catch (error) {
         res.status(400).json({ error: error.message });
