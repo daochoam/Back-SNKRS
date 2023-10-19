087bb7d3fe8839091f090f6eb37f3da0c96e1598
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2023-10-19 12:09:22 -0500

    Fix filter in getAllUser and Add res in getSales

--- src/controllers/admin/getAllUser.js
+++ src/controllers/admin/getAllUser.js
@@ -6,12 +6,15 @@ const getAllUser = async (req, res) => {
         const query = {};
         const sort = {};
         if (country) query["address.0.country"] = new RegExp(country, 'i');
-        if (status) query.status = new RegExp(status, 'i');
+        if (status) query.status = status
         if (firstName) sort.firstName = firstName;
         if (email) sort.email = email;
 
         const users = await User.find(query).sort(sort);
+        console.log("users", users.length)
+
         res.status(200).json(users);
+
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
